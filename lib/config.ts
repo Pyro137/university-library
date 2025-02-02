eef1b92da1bf879ff75331f212807381146ce57d
Pyro137 <126709211+Pyro137@users.noreply.github.com>
2025-02-02 20:54:06 +0300

    email gönderemedik

--- lib/config.ts
+++ lib/config.ts
@@ -1,28 +1,35 @@
-const config={
-    env:{
-        databaseUrl:process.env.DATABASE_URL,
-        prodEndPoint:process.env.PROD_ENDPOINT,
-        apiEndPoint:process.env.NEXT_PUBLIC_API_ENDPOINT,
-        imagekit:{
-            publicKey:process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
-            privateKey:process.env.IMAGEKIT_PRIVATE_KEY,
-            urlEndPoint:process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT
-
+const config = {
+    env: {
+      databaseUrl: process.env.DATABASE_URL,
+      prodEndPoint: process.env.PROD_ENDPOINT,
+      apiEndPoint: process.env.NEXT_PUBLIC_API_ENDPOINT,
+      imagekit: {
+        publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+        privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+        urlEndPoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+      },
+      upstash: {
+        redisUrl: process.env.UPSTASH_REDIS_URL,
+        redisToken: process.env.UPSTASH_REDIS_TOKEN,
+        qstashUrl: process.env.QSTASH_URL,
+        qstashToken: process.env.QSTASH_TOKEN,
+      },
+      emailjs: {
+        emailjsServiceId: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+        emailjsTemplateId: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        emailjsPublicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!,
+      },
+      resendToken: process.env.RESEND_TOKEN,
+      nodemailer:{
+        host:process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD,
         },
-        upstash:{
-            redisUrl:process.env.UPSTASH_REDIS_URL,
-            redisToken:process.env.UPSTASH_REDIS_TOKEN,
-            qstashUrl:process.env.QSTASH_URL,
-            qstashToken:process.env.QSTASH_TOKEN
-        },
-        emailjs:{
-            serviceId:process.env.EMAILJS_SERVICE_ID,
-            templateId:process.env.EMAILJS_TEMPLATE_ID,
-            publicKey:process.env.EMAILJS_PUBLIC_KEY
-        },
-        resendToken:process.env.RESEND_TOKEN
-
+      }
     },
-}
-
-export default config;
+  };
+  
+  export default config;
+  
